d603e1be6d8e965493776a45d4b429f2e4c5b081
e <hichenning@gmail.com>
2016-12-31 20:34:33 +0800

    add new feature

---
+++ src/main/java/cn/com/model/Admin.java
@@ -0,0 +1,28 @@
+package cn.com.model;
+
+public class Admin {
+	
+	private String name;
+	private String password;
+	private int leval;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public int getLeval() {
+		return leval;
+	}
+	public void setLeval(int leval) {
+		this.leval = leval;
+	}
+	 
+
+}
